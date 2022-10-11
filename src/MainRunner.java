cfdfa0b08395f42db41efb41171960bd4fe852cf
student <bonniec15@nycstudents.net>
2022-10-11 10:53:17 -0400

    Initial Commit

---
+++ src/MainRunner.java
@@ -0,0 +1,16 @@
+public class MainRunner {
+    public static void main(String[] args){
+        String blah = "What's for dinner?";
+        String ugh = blah.substring(11,17);
+        System.out.println(ugh);
+
+        String blah2 = "What's for dinner?";
+        String ugh2 = blah.substring(11);
+        String ugh3 = blah.substring(11,18);
+        System.out.println(ugh2);
+        System.out.println(ugh3);
+
+
+
+    }
+}
